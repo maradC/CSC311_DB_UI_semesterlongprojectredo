f3e5bf8e132a4885e0b3dff08e7d8070ae81aade
maradC <maradiagachristopher5@gmail.com>
2024-12-02 13:35:09 -0500

    working on image for signUP

--- src/main/java/viewmodel/MainApplication.java
+++ src/main/java/viewmodel/MainApplication.java
@@ -13,7 +13,7 @@ import javafx.util.Duration;
 public class MainApplication extends Application {
 
     private static Scene scene;
-    private static DbConnectivityClass cnUtil;
+    public static DbConnectivityClass cnUtil;
     private Stage primaryStage;
 
     public static void main(String[] args) {
@@ -23,11 +23,11 @@ public class MainApplication extends Application {
     }
 
     public void start(Stage primaryStage) {
-        Image icon = new Image(getClass().getResourceAsStream("/images/DollarClouddatabase.png"));
+        Image icon = new Image(getClass().getResourceAsStream("/images/logo.png"));
         this.primaryStage = primaryStage;
         this.primaryStage.setResizable(false);
         primaryStage.getIcons().add(icon);
-        primaryStage.setTitle("FSC CSC311 _ Database Project");
+        primaryStage.setTitle("Soccer Students");
         showScene1();
     }
 
